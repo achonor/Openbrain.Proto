548210a1f93744fd1b764d10525955aeeef521b0
lilincong <lidaha.004@hotmail.com>
2018-07-24 18:34:09 +0800

    添加字段

--- xls2protobuf_v3.bat
+++ xls2protobuf_v3.bat
@@ -1,4 +1,5 @@
 python xls2protobuf_v3.py xls
 python copyfile.py xls2bin ../Openbrain.Client/Assets/StreamingAssets/DataConfig
+python copyfile.py xls2bin ../Openbrain.Server/DataConfig
 python protoc.py xls2proto/
 pause
